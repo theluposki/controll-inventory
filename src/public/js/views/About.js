48ee258e08a72613d4bcb1706641c59a8e5b49a2
root <root@localhost.localdomain>
2022-09-29 22:42:03 -0300

    dexie config

--- src/public/js/views/About.js
+++ src/public/js/views/About.js
@@ -1,3 +1,31 @@
+import db from "../db.js"
+
 export const About = {
-    template: `<h1>About</h1>`
+    template: 
+`
+<div class="about">
+  <ul class="list">
+    <li v-for="(item, index) in list" class="l-item" :key="index">
+       <h4>{{ item.name }}</h4>
+       <span>{{ formatCurrency(item.preco) }}</span>
+    </li>
+  </ul>
+</div>
+`,
+data() {
+  return {
+    list: []
+  }
+},
+async mounted() {
+ const products = await db.products.toArray()
+ this.list = products
+},
+methods: {
+  formatCurrency(number) {
+    return new Intl.NumberFormat('pt-br', { 
+        style: 'currency', currency: 'BRL' 
+    }).format(number)
+  }
+}
 }
